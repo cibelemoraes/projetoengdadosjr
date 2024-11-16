4538cf7d3caccc20ffa42133672d4ffb05f9cc41
Your Name <you@example.com>
2024-11-15 22:28:54 -0300

    bigquery

--- src/load/load_to_bigquery.py
+++ src/load/load_to_bigquery.py
@@ -6,10 +6,20 @@ from pandas_gbq import to_gbq
 # Carregue suas credenciais
 
 credencial = service_account.Credentials.from_service_account_file(
-    r'D:\biblioteca_projeto\meu-primeiro-projet-403011-f594f7373847.json',
-    scopes=['https://oauth2.googleapis.com/token']
+    r'D:\\biblioteca_projeto\\meu-primeiro-projet-403011-c7fe55c69300.json',
+    scopes=['https://www.googleapis.com/auth/bigquery']
 )
 # carregando o arquivo csv
 df = pd.read_csv(r"D:\biblioteca_projeto\steam_sales.csv")
 df.head()
 #print(df.head())
+
+#Envie o DataFrame para o BigQuery
+df.to_gbq(destination_table='meu-primeiro-projet-403011.MEUPRIMEIRO.steam_sales', # substitua pelo nome correto da sua tabela nesse steam_sales
+          project_id='meu-primeiro-projet-403011',
+          if_exists='replace', 
+          credentials=credencial)
+
+#replace igual a substituir antiga pela original
+#append = adiciona dados no fim da tabela 
+
