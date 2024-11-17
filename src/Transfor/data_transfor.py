57fdb0e8f3928484ab3f9021b02ceacc4a39bebf
Your Name <you@example.com>
2024-11-17 13:54:17 -0300

    data transformer

--- src/Transfor/data_transfor.py
+++ src/Transfor/data_transfor.py
@@ -7,7 +7,7 @@ df = pd.read_csv('D:\\biblioteca_projeto\\steam_sales.csv')
 
 
 #Removendo Linhas Vazias
-df = df.dropna()
+df = df.dropna(subset=['Name', 'Release'])
 
 
 #funcao para converter datas
@@ -15,7 +15,7 @@ def converte_datas(data):
     meses = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 
              'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
     
-    if pd.isna(data):
+    if pd.isna(data) or len(data.split()) != 2:  # Verifica se é NaN ou não tem 2 partes
         return None
     mes, ano = data.split()
     return f"{meses[mes]}/{ano}"
@@ -26,10 +26,11 @@ df['Release'] = df['Release'].apply(converte_datas)
 #criando uma coluna para os jogos gratis 
 df['Jogos Gratis'] = df['Price'].apply(lambda x: 'free' if x== 'R$ 0,00' else '')
 
+#print(df['Release'].head())
 #print(df)
 
 # Salvando o DataFrame modificado
-df.to_csv('D:\\biblioteca_projeto\\steam_sales_copy.csv', index=False)
+df.to_csv('D:\\biblioteca_projeto\\data_transfor.csv', index=False)
     
 
 
